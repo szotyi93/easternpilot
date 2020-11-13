5034591225f30c9fb1cb342ca2030915da110e0e
Shane Smiskol <shane@smiskol.com>
2020-11-13 02:56:40 -0600

    Add hacky derivative, just put your k_d value in the file

--- selfdrive/controls/lib/pid.py
+++ selfdrive/controls/lib/pid.py
@@ -1,5 +1,10 @@
 import numpy as np
 from common.numpy_fast import clip, interp
+import os
+use_derivative, k_d = os.path.exists('/data/use_deriv'), 0
+if use_derivative:
+  with open('/data/use_deriv', 'r') as f:
+    k_d = float(f.read().strip())
 
 def apply_deadzone(error, deadzone):
   if error > deadzone:
@@ -54,19 +59,24 @@ class PIController():
     self.sat_count = 0.0
     self.saturated = False
     self.control = 0
+    self.last_error = 0
 
   def update(self, setpoint, measurement, speed=0.0, check_saturation=True, override=False, feedforward=0., deadzone=0., freeze_integrator=False):
     self.speed = speed
 
     error = float(apply_deadzone(setpoint - measurement, deadzone))
     self.p = error * self.k_p
+    d = 0
+    if use_derivative:
+      d = k_d * (error - self.last_error)
+
     self.f = feedforward * self.k_f
 
     if override:
       self.i -= self.i_unwind_rate * float(np.sign(self.i))
     else:
       i = self.i + error * self.k_i * self.i_rate
-      control = self.p + self.f + i
+      control = self.p + self.f + i + d
 
       if self.convert is not None:
         control = self.convert(control, speed=self.speed)
@@ -78,11 +88,12 @@ class PIController():
          not freeze_integrator:
         self.i = i
 
-    control = self.p + self.f + self.i
+    control = self.p + self.f + self.i + d
     if self.convert is not None:
       control = self.convert(control, speed=self.speed)
 
     self.saturated = self._check_saturation(control, check_saturation, error)
+    self.last_error = float(error)
 
     self.control = clip(control, self.neg_limit, self.pos_limit)
     return self.control
