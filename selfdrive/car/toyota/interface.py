2313ed78cbf1994a3c8dff09ffcce4b1f32c4a4d
Shane Smiskol <shane@smiskol.com>
2020-11-13 20:32:02 -0600

    Corolla INDI tuning suggestions from zorro

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -73,7 +73,7 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.init('indi')
       ret.lateralTuning.indi.innerLoopGain = 4.0
       ret.lateralTuning.indi.outerLoopGain = 3.0
-      ret.lateralTuning.indi.timeConstant = 0.1
+      ret.lateralTuning.indi.timeConstant = 0.5
       ret.lateralTuning.indi.actuatorEffectiveness = 1.0
 
     elif candidate == CAR.LEXUS_RX:
@@ -268,7 +268,7 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.35], [0.15]]
       ret.lateralTuning.pid.kf = 0.00007818594
 
-    ret.steerRateCost = 0.5
+    ret.steerRateCost = 0.7
     ret.centerToFront = ret.wheelbase * 0.44
 
     # TODO: get actual value, for now starting with reasonable value for
